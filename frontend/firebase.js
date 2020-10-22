d771ca636628d912c74e0035d821e92ff9c7287e
harpreet-singh-au7 <simransingh12340@gmail.com>
2020-10-22 18:30:46 +0530

    final dep

---
+++ frontend/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCGw0ypa3Z6x1RRD1Cx8vAOzVT5zjkbq2o",
+    authDomain: "random-a83fa.firebaseapp.com",
+    databaseURL: "https://random-a83fa.firebaseio.com",
+    projectId: "random-a83fa",
+    storageBucket: "random-a83fa.appspot.com",
+    messagingSenderId: "229329259317",
+    appId: "1:229329259317:web:90a7caeb0d366ba2a65a7c"
+  };
